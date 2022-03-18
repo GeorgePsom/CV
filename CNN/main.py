2f7621ee230214d974f2dfca3bc7da95d964a62b
spiliour <32042882+spiliour@users.noreply.github.com>
2022-03-18 14:41:34 +0100

    Revert "running sample"
    This reverts commit a185217f06a7ffa0bf04d0b54cf53c9e569ea683.

--- CNN/main.py
+++ CNN/main.py
@@ -1,11 +1,10 @@
-# TensorFlow and tf.keras
 import tensorflow as tf
 
-# Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
 print(tf.__version__)
+
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 
@@ -14,49 +13,47 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-train_images.shape
+print(train_images.shape)
 
-len(train_labels)
+print(len(train_labels))
 
-train_labels
-
-test_images.shape
-
-len(test_labels)
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
 
 train_images = train_images / 255.0
-
 test_images = test_images / 255.0
 
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+
 model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPool2D(),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
     tf.keras.layers.Dense(10)
 ])
-
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=10)
+model.fit(train_images, train_labels, batch_size= 128, epochs=15)
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
-print('\nTest accuracy:', test_acc)
-
-probability_model = tf.keras.Sequential([model,
-                                         tf.keras.layers.Softmax()])
-
-predictions = probability_model.predict(test_images)
-
-predictions[0]
-
-np.argmax(predictions[0])
-
-test_labels[0]
-
-
-
-
-
-
+print('\nTest accuracy: ', test_acc)
