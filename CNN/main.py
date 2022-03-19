6b3013b50ae76500f96a896c81432236c87e203a
George Psomathianos <45467851+GeorgePsom@users.noreply.github.com>
2022-03-19 10:52:51 +0100

    CNN base

--- CNN/main.py
+++ CNN/main.py
@@ -1,4 +1,9 @@
 import tensorflow as tf
+import sklearn.model_selection as sk_ms
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+from tensorflow.keras.losses import sparse_categorical_crossentropy
+from tensorflow.keras.optimizers import Adam
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -25,7 +30,8 @@ print(len(train_labels))
 
 train_images = train_images / 255.0
 test_images = test_images / 255.0
-
+acc_per_fold = []
+loss_per_fold = []
 # plt.figure(figsize=(10,10))
 # for i in range(25):
 #     plt.subplot(5,5,i+1)
@@ -36,24 +42,61 @@ test_images = test_images / 255.0
 #     plt.xlabel(class_names[train_labels[i]])
 # plt.show()
 
+kfold = sk_ms.KFold(n_splits = 5, shuffle = True)
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.MaxPool2D(),
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(10)
-])
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+fold_no = 1
 
-model.fit(train_images, train_labels, batch_size= 128, epochs=15)
+for train, test in kfold.split(train_images, train_labels):
+    #Define Model Architecture
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size = (3, 3), activation = 'relu', input_shape = (28, 28, 1)))
+    model.add(MaxPooling2D(pool_size = (2, 2)))
 
-test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+    model.add(Conv2D(64, kernel_size = (3, 3), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size = (2, 2)))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation = 'relu'))
+    model.add(Dense(64, activation = 'relu'))
+    model.add(Dense(10))
 
-print('\nTest accuracy: ', test_acc)
+    model.compile(optimizer = Adam(learning_rate = 1e-3), loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits= True), metrics = ['accuracy'], )
+    print('------------------------------------------------------------------------')
+    print(f'Training for fold {fold_no} ...')
+    history = model.fit(train_images[train], train_labels[train], batch_size = 128, epochs = 15, verbose = 2, )
+
+    scores = model.evaluate(train_images[test], train_labels[test], verbose = 0)
+    print(f'Score for fold {fold_no}: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%')
+    acc_per_fold.append(scores[1] * 100)
+    loss_per_fold.append(scores[0])
+
+    # Increase fold number
+    fold_no = fold_no + 1
+
+# == Provide average scores ==
+print('------------------------------------------------------------------------')
+print('Score per fold')
+for i in range(0, len(acc_per_fold)):
+  print('------------------------------------------------------------------------')
+  print(f'> Fold {i+1} - Loss: {loss_per_fold[i]} - Accuracy: {acc_per_fold[i]}%')
+print('------------------------------------------------------------------------')
+print('Average scores for all folds:')
+print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
+print(f'> Loss: {np.mean(loss_per_fold)}')
+print('------------------------------------------------------------------------')
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+#     tf.keras.layers.Flatten(),
+#     tf.keras.layers.Dense(64, activation='relu'),
+#     tf.keras.layers.Dense(10)
+# ])
+# model.compile(optimizer='adam',
+#               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+#               metrics=['accuracy'])
+#
+# model.fit(train_images, train_labels, batch_size= 128, epochs=15)
+#
+# test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+#
+# print('\nTest accuracy: ', test_acc)
