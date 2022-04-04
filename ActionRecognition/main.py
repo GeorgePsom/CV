a415c29f14a3601de1fe3ccae7af34053096c417
spiliour <32042882+spiliour@users.noreply.github.com>
2022-04-04 17:54:58 +0200

    data import

--- ActionRecognition/main.py
+++ ActionRecognition/main.py
@@ -1,15 +1,18 @@
 import wget
 import unzip
-#import cv2
+import cv2
 import numpy as np
 from helpfuncs import*
 from sklearn.model_selection import StratifiedShuffleSplit
-import tensorflow as tf
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import sklearn.model_selection as sk_ms
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
 from tensorflow.keras.losses import sparse_categorical_crossentropy
 from tensorflow.keras.optimizers import Adam
+from PIL import Image
 
 # url1 = 'http://vision.stanford.edu/Datasets/Stanford40_JPEGImages.zip'
 #
@@ -33,11 +36,64 @@ with open('ImageSplits/test.txt', 'r') as f:
     #print(f'Test labels ({len(test_labels)}):\n\t{test_labels}\n')
 
 action_categories = sorted(list(set(['_'.join(name.split('_')[:-1]) for name in train_files])))
-#print(f'Action categories ({len(action_categories)}):\n{action_categories}')
+print(action_categories)
+
+categ_dict = {}
+i=0
+
+for action in action_categories:
+    categ_dict[action] = i
+    i += 1
+
+for i in range(0,len(train_labels)):
+    train_labels[i] = categ_dict[train_labels[i]]
+
+for i in range(0,len(test_labels)):
+    test_labels[i] = categ_dict[test_labels[i]]
+
 
 X_train, Y_train, X_validation, Y_validation = stratify_data(train_files, train_labels)
 
 stratification_check(Y_train, Y_validation)
+
+
+IMAGE_SIZE = 112
+CHANNELS = 3
+
+def tf_resize_images(X_img_file_paths):
+    X_data = []
+    tf.compat.v1.reset_default_graph()
+    X = tf.placeholder(tf.float32, (None, None, CHANNELS))
+    tf_img = tf.image.resize_images(X, (IMAGE_SIZE, IMAGE_SIZE),
+                                    tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        # Each image is resized individually as different image may be of different size.
+        for index, file_path in enumerate(X_img_file_paths):
+            #print(file_path)
+            img = Image.open('JPEGImages/'+file_path)
+            #print(np.shape(np.shape(img)))
+            if len(np.shape(img))!=3:
+                img2 = np.zeros((np.shape(img)[0],np.shape(img)[1],3))
+                img2[:, :, 0] = img
+                img2[:, :, 1] = img
+                img2[:, :, 2] = img
+                resized_img = sess.run(tf_img, feed_dict={X: img2})
+            else:
+                resized_img = sess.run(tf_img, feed_dict = {X: img})
+            X_data.append(resized_img)
+
+    X_data = np.array(X_data, dtype = np.float32) # Convert to numpy
+    return X_data
+
+X_train_images = tf_resize_images(X_train)
+
+X_validation_images = tf_resize_images(X_validation)
+
+#X_train_images = tf_resize_images(train_files)
+
+#X_test_images = tf_resize_images(test_files)
 
 
 model = Sequential()
@@ -50,13 +106,16 @@ model.add(MaxPooling2D(pool_size = (2, 2)))
 model.add(Flatten())
 model.add(Dense(128, activation = 'relu'))
 model.add(Dense(64, activation = 'relu'))
-model.add(Dense(10))
+model.add(Dense(40))
 
 model.compile(optimizer = Adam(), loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits= True), metrics = ['accuracy'], )
-history = model.fit(X_train, Y_train, batch_size = 128, epochs = 15, verbose = 2)
+#history = model.fit(X_train_images, train_labels, batch_size = 128, epochs = 15, verbose = 2)
+history = model.fit(X_train_images, Y_train, batch_size = 128, epochs = 15, verbose = 2)
 
-#scores = model.evaluate(test_files, test_labels, verbose = 2)
+#scores = model.evaluate(X_test_images, test_labels, verbose = 2)
+scores = model.evaluate(X_validation_images, Y_validation, verbose = 2)
 
+print(f'Score for fold: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%')
 
 
 
